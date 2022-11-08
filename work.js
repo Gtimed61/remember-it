3681265eb5b9af2180748fa0a094ff946f87bff0
Grant Thorne <gthorne61@gmail.com>
2022-11-07 22:07:32 -0600

    changes

--- work.js
+++ work.js
@@ -1,49 +1,31 @@
-//open, shows day and current time -momentjs
-
-
-
-//time blocks for 9-5
-
-
-
-//time blocks color coded for past-red present- future-
-//timeblocks on click add task
-
 var tasks = {};
 
-var createTask = function(taskText, taskDate, taskList) {
-    // create elements that make up a task item
-    var taskLi = $("<li>").addClass("list-group-item");
-    var taskSpan = $("<span>")
-      .addClass("badge badge-primary badge-pill")
-      .text(taskDate);
-    var taskP = $("<p>")
-      .addClass("m-1")
-      .text(taskText);
-  
-    // append span and p element to parent li
-    taskLi.append(taskSpan, taskP);
-
-
-     // append to ul list on the page
-  $("#list-" + taskList).append(taskLi);
-
-//save button for timeblocks save task to local storage
-
-var saveTasks = function() {
+//function to create task on click of button
+const btn = document.getElementsByClassName('btn')
+    //allow clicked button to create task
+    var taskNote = document.querySelector('#taskNote').value
+    tasks[""] = taskNote;
+    //append to html object
+    var taskList = document.getElementById("button");
+    var task = document.createElement("li");
+    task.innerHTML = taskNote;
+    taskNote.appendChild(tasks);
+    console.log(tasks);
+    //clear input field
+    document.querySelector('#taskNote').value = "";
+    //add task to list
+    taskList.appendChild(task);
+    //add task to local storage
     localStorage.setItem("tasks", JSON.stringify(tasks));
-  };
-//on refresh, saved task persist
 
-var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  
-    // if nothing in localStorage, create a new object to track all task status arrays
-    if (!tasks) {
-      tasks = { //save to selected 
-        toDo: [],
-        inProgress: [],
-        inReview: [],
-        done: []
-      };
-    }
+
+
+
+btn.addEventListener('click', function () {
+    var taskToDo = document.getElementById("taskToDo").value;
+    var taskNote = document.getElementById("taskNote").value;
+    var taskList = document.getElementById("taskList");
+    var task = document.createElement("li");
+    task.innerHTML = taskToDo + " " + taskNote;
+    taskList.appendChild(task);
+})
