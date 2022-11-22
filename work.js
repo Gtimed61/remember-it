0b72778c0454eedf0e7e9ebff7ec062aee987fe2
Grant Thorne <gthorne61@gmail.com>
2022-11-22 11:36:29 -0600

    timeblocks revised

--- work.js
+++ work.js
@@ -1,31 +1,12 @@
 var tasks = {};
 
-//function to create task on click of button
-const btn = document.getElementsByClassName('btn')
-    //allow clicked button to create task
-    var taskNote = document.querySelector('#taskNote').value
-    tasks[""] = taskNote;
-    //append to html object
-    var taskList = document.getElementById("button");
-    var task = document.createElement("li");
-    task.innerHTML = taskNote;
-    taskNote.appendChild(tasks);
-    console.log(tasks);
-    //clear input field
-    document.querySelector('#taskNote').value = "";
-    //add task to list
-    taskList.appendChild(task);
-    //add task to local storage
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+//create task element and append to the DOM
+tasks.create = function (task) {
+    var taskElement = document.createElement('div');
+    taskElement.className = 'task';
+    taskElement.innerHTML = task;
+    document.getElementById('tasks').appendChild(taskElement);
+}
 
 
 
-
-btn.addEventListener('click', function () {
-    var taskToDo = document.getElementById("taskToDo").value;
-    var taskNote = document.getElementById("taskNote").value;
-    var taskList = document.getElementById("taskList");
-    var task = document.createElement("li");
-    task.innerHTML = taskToDo + " " + taskNote;
-    taskList.appendChild(task);
-})
